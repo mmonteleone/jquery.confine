2008dfc73bc2b408b615d783163b5259e5670532
Michael Monteleone <michael@michaelmonteleone.net>
2009-11-28 09:16:39 -0600

    added unit tests with autotype, pavlov, delorean

---
+++ spec/jquery.confine.specs.js
@@ -0,0 +1,334 @@
+QUnit.specify("jQuery.confine", function() {
+    // setup some helpers        
+
+    // simulates the behavior of a paste
+    $.fn.simulatePaste = function(value) {
+        return this.each(function(){
+            var area = $(this);
+            area.trigger('paste')       // triggers event first
+                .val(area.val()+value); // and then changes value (just like real)
+        });
+    }
+    
+    var fakeGlobal = {
+        setTimeout: DeLorean.setTimeout
+    };
+
+    // shortcut for building up and breaking down stub forms
+    var FormBuilder = {
+        clear: function(){
+            $('div#testbed form').empty();
+            $('textarea').die('paste');
+            $('textarea').die('keydown');
+        },
+        addTextArea: function(name, value){
+            var input = $('<textarea class="test" name="' + name + '" id="' + name + '">' + value + '</textarea>');
+            $('div#testbed form').append(input);
+            return input;
+        }
+    };
+    
+    describe("jQuery.confine", function(){
+        it("should be equivalent to calling jQuery( jQuery.fn.confine.defaults.selector ).confine( options );", function(){
+            FormBuilder.addTextArea("text1","text1val1");
+            FormBuilder.addTextArea("text2","text2val2");
+            FormBuilder.addTextArea("text3","text3val3");
+
+            var originalConfine = $.fn.confine;
+            try{
+                var passedOptions;
+                var selection;
+                $.fn.confine = function(opts) {
+                    passedOptions = opts;                                        
+                    selection = this;
+                };
+                $.extend($.fn.confine, {defaults:originalConfine.defaults});
+                var someOpts = {a:1,b:2};
+                $.confine(someOpts);
+                assert(someOpts).isSameAs(passedOptions);
+                assert(selection.size()).equals(3);  
+            } finally {
+                $.fn.confine = originalConfine;
+            }         
+        });            
+    });
+            
+    describe("defaults", function(){
+        it("should have a value of 'window' for global", function(){
+            assert($.fn.confine.defaults.global).isSameAs(window);
+        });
+    });
+    describe("upon initial activation", function(){
+        before(function(){
+            FormBuilder.addTextArea('t1','abcde');                
+            FormBuilder.addTextArea('t2','abcde');                
+            FormBuilder.addTextArea('t3','abcde');                
+        });
+        
+        after(function(){
+            FormBuilder.clear();
+        });
+        
+        describe("when textareas default to containing less than the max length", function(){
+            it("should not raise maxlength event", function(){
+                var maxLengthRaised = false;
+                $('textarea')
+                    .bind('maxlength', function(){
+                        maxLengthRaised = true;                            
+                    })
+                    .confine({maxlength: 10});
+                assert(maxLengthRaised).isFalse();
+            });
+            it("should not clip the content", function(){
+                $('textarea').confine({maxlength: 10});
+                assert($('textarea').val()).equals('abcde');
+            });
+        });
+        describe("when textareas default to containing precisely the max length", function(){
+            it("should not raise maxlength event", function(){
+                var maxLengthRaised = false;
+                $('textarea')
+                    .bind('maxlength', function(){
+                        maxLengthRaised = true;                            
+                    })
+                    .confine({maxlength: 5});
+                assert(maxLengthRaised).isFalse();                    
+            });
+            it("should not clip the content", function(){
+                $('textarea').confine({maxlength: 5});
+                assert($('textarea').val()).equals('abcde');                    
+            });
+        });
+        describe("when textareas default to containing over the max length", function(){
+            it("should raise maxlength event", function(){
+                var maxLengthRaised = false;
+                $('textarea')
+                    .bind('maxlength', function(){
+                        maxLengthRaised = true;                            
+                    })
+                    .confine({maxlength:3});
+                assert($('textarea').val()).equals('abc');
+                assert(maxLengthRaised).isTrue();
+            });
+            it("should clip the content to be same length as maxlength", function(){
+                $('textarea').confine({maxlength:3});
+                assert($('textarea').val()).equals('abc');
+            });
+        });
+    });
+    describe("upon typing", function(){
+        before(function(){
+            FormBuilder.addTextArea('t1','abcde');                
+            FormBuilder.addTextArea('t2','abcde');                
+            FormBuilder.addTextArea('t3','abcde');                
+        });
+        
+        after(function(){
+            FormBuilder.clear();
+        });
+
+        describe("that results in length under the max", function(){                
+            it("should not raise maxlength event", function(){
+                var maxLengthRaised = false;
+                $('textarea')
+                    .bind('maxlength', function(){
+                        maxLengthRaised = true;                            
+                    })                        
+                    .confine({maxlength: 10})
+                    .autotype('xyz');
+                assert(maxLengthRaised).isFalse();
+            });
+            it("should modify the contents as normal", function(){
+                $('textarea')
+                    .confine({maxlength: 10})
+                    .autotype('xyz');
+                assert($('textarea').val()).equals('abcdexyz');
+            });
+        });
+        describe("that results in precisely max length", function(){
+            it("should not raise maxlength event", function(){
+                var maxLengthRaised = false;
+                $('textarea')
+                    .bind('maxlength', function(){
+                        maxLengthRaised = true;                            
+                    })                        
+                    .confine({maxlength: 10})
+                    .autotype('vwxyz');
+                assert(maxLengthRaised).isFalse();                    
+            });
+            it("should modify the contents as normal", function(){
+                $('textarea')
+                    .confine({maxlength: 10})
+                    .autotype('vwxyz');
+                assert($('textarea').val()).equals('abcdevwxyz');
+            });
+        });
+        describe("that would result in over max length", function(){
+            it("should raise maxlength event on each overage attempt", function(){
+                var events=[];
+                $('textarea')
+                    .bind('maxlength', function(e){                            
+                        events.push(e);
+                    })                        
+                    .confine({maxlength: 10})
+                    .autotype('vwxyz123');
+                assert(events.length).equals(9);
+            });
+            it("should not modify contents as normal", function(){
+                $('textarea')
+                    .confine({maxlength: 10})
+                    .autotype('vwxyz');
+                assert($('textarea').val()).equals('abcdevwxyz');                    
+            });
+            it("should still allow control keys to be entered without raising maxlength", function(){
+                var events=[];
+                $('textarea')
+                    .bind('maxlength', function(e){
+                        events.push(e);
+                    })                        
+                    .confine({maxlength: 10})
+                    .autotype('vwxyz{{ctrl}}{{shift}}{{/shift}}{{left}}{{back}}{{up}}');
+                assert(events.length).equals(0);
+            });
+            it("should still allow combo of modifier keys with character entries without raising maxlength", function(){
+                var events=[];
+                $('textarea')
+                    .bind('maxlength', function(e){
+                        events.push(e);
+                    })                        
+                    .confine({maxlength: 10})
+                    .autotype('vwxyz{{ctrl}}a{{/ctrl}}{{meta}}a{{/meta}}{{ctrl}}c{{/ctrl}}');
+                assert(events.length).equals(0);                    
+            });
+        });
+    });
+    describe("upon pasting", function(){
+        before(function(){
+            FormBuilder.addTextArea('t1','abcde');                
+            FormBuilder.addTextArea('t2','abcde');                
+            FormBuilder.addTextArea('t3','abcde');                
+        });
+        
+        after(function(){
+            FormBuilder.clear();
+        });            
+        
+        describe("that results in length under the max", function(){
+            describe("immediately", function(){
+                it("should not raise maxlength event", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength: 10})
+                        .simulatePaste('xyz');
+                    assert(events.length).equals(0);
+                });
+            });
+            describe("after 2 ms", function(){
+                it("should not have raised maxlength event", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz');
+                    DeLorean.advance(10);
+                    assert(events.length).equals(0);                                                        
+                });
+                it("should modify the contents as normal", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz');
+                    DeLorean.advance(10);
+                    assert($('textarea').val()).equals('abcdexyz');
+                });                    
+            });
+        });
+        describe("that results in precisely max length", function(){
+            describe("immediately", function(){
+                it("should not raise maxlength event", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz12');
+                    assert(events.length).equals(0);     
+                    DeLorean.advance(10);                   
+                });
+            });
+            describe("after 1 ms", function(){
+                it("should not raise maxlength event", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz12');
+                    DeLorean.advance(10);
+                    assert(events.length).equals(0);                                                        
+                });
+                it("should modify the contents as normal", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz12');
+                    DeLorean.advance(10);
+                    assert($('textarea').val()).equals('abcdexyz12');
+                });                    
+            });                
+        });
+        describe("that would result in over max length", function(){
+            describe("immediately", function(){
+                it("should not raise maxlength event", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz1234');
+                    assert(events.length).equals(0);
+                });
+            });
+            describe("after 1 ms", function(){
+                it("should raise maxlength event", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz12345');
+                    DeLorean.advance(10)
+                    assert(events.length).equals(3);
+                });
+                it("should clip contents", function(){
+                    var events = [];
+                    $('textarea')
+                        .bind('maxlength', function(e){
+                            events.push(e);
+                        })
+                        .confine({maxlength:10, global:fakeGlobal})
+                        .simulatePaste('xyz12');
+                    DeLorean.advance(10);
+                    assert($('textarea').val()).equals('abcdexyz12');
+                });
+            });
+        });
+    });
+       
+});
+
